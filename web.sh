0383e051db47d9a21f19090f4ad66a097310acca
fanxiaolong <developer@lunarhook.com>
2024-05-07 01:28:58 +0800

    u

--- web.sh
+++ web.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 export PATH=$PATH:/usr/local/go/bin
 git pull
-cd server/Global/main/
-go build -o Russiamain main.go 
-cd ..
+cd src/main/
+go build -o game900top main.go
 cd ..
 cd ..
 
-ps -ef |grep Russiamain | grep -v grep |awk '{print $2}' | xargs kill -9
-#cp -Rf ./server/Global/main/Russiamain ./Russiamain
-nohup ./Russiamain >Russiamain.out 2>&1 &
+ps -ef |grep game900top | grep -v grep |awk '{print $2}' | xargs kill -9
+cp -Rf ./src/main/game900top ./game900top
+nohup ./game900top >/dev/null 2>&1 &
